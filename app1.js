c257af96f0c87333beca54d2326ffb28a26daf92
nikhilmahato104 <nikhilmahato104@gmail.com>
2024-10-12 05:00:30 +0530

    abc

--- app1.js
+++ app1.js
@@ -4,4 +4,8 @@ console.log("jai shree krishna");
 
 
 console.log("jai shree ram");
+<<<<<<< HEAD
 console.log("jai shree krishna"); //editing by prity
+=======
+console.log("jai shree krishna"); //edit by prity
+>>>>>>> prity
---
+++ prity.js
@@ -0,0 +1,3 @@
+{
+    alert("hey i am prity");
+}
